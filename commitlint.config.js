e9d7d56015dd766835dd03242bd6d427aa39820b
liyang <yang.li@31abconline.com>
2019-09-09 14:26:20 +0800

    chore: 新增开发依赖 husky commitlint，开启提交信息的规范检测

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: liyang
+ * @Date: 2019-09-09 14:17:02
+ * @LastEditTime: 2019-09-09 14:18:57
+ * @LastEditors: liyang
+ * @Description: commitlint 配置文件
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
